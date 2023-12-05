cb75eaa1a61b2d73fb458555da2d08e172078d51
V-Matheus <victormatheus507@gmail.com>
2023-12-05 15:46:31 -0300

    organizacao

---
+++ redux basico/organizacao/store/contador.js
@@ -0,0 +1,23 @@
+// Constantes
+
+const INCREMENTAR = 'contador/INCREMENTAR';
+const REDUZIR = 'contador/REDUZIR';
+
+export const incrementar = () => ({ type: INCREMENTAR });
+export const reduzir = () => ({ type: REDUZIR });
+
+// Reducer
+
+const initialState = 0;
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case INCREMENTAR:
+      return state + 1;
+    case REDUZIR:
+      return state - 1;
+    default:
+      return state;
+  }
+};
+
+export default reducer;
