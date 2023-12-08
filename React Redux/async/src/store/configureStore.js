aa63745e5d2a1fea433bad1f545e3269a3df42a8
V-Matheus <victormatheus507@gmail.com>
2023-12-08 18:10:15 -0300

    async

---
+++ React Redux/async/src/store/configureStore.js
@@ -0,0 +1,16 @@
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import contador from './contador';
+import modal from './modal';
+import login from './login';
+// import logger from './middleware/logger';
+
+// const middleware = [logger];
+
+const reducer = combineReducers({ contador, modal, login });
+const store = configureStore({
+  reducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(),
+});
+
+export default store;
