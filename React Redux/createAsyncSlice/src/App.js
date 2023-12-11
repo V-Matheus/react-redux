f9258087db26065290e2370b5b642b4074ffcddc
V-Matheus <victormatheus507@gmail.com>
2023-12-11 16:18:10 -0300

    createAsyncSlice

---
+++ React Redux/createAsyncSlice/src/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {useDispatch} from 'react-redux'
+import { login } from './store/login';
+
+function App() {
+  const [username, setUsername] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  const dispatch = useDispatch()
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    dispatch(login({username, password}))
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label style={{ displey: 'block' }} htmlFor="username">
+          Usuário
+        </label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={({ target }) => setUsername(target.value)}
+        />
+        <br />
+        <label style={{ displey: 'block' }} htmlFor="password">
+          Senha
+        </label>
+        <input
+          type="password"
+          id="username"
+          value={password}
+          onChange={({ target }) => setPassword(target.value)}
+          />
+          <button>Enviar</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
