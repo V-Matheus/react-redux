44b738d6ce979abbafdeec8ea8cd6efc970fcdbe
V-Matheus <victormatheus507@gmail.com>
2023-12-12 16:43:42 -0300

    desafio parte 1

---
+++ React Redux/desafio/src/store/configureStore.js
@@ -0,0 +1,15 @@
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import login from './login';
+
+import localStorage from './middleware/localStorage';
+
+
+const local = localStorage;
+const reducer = combineReducers({login });
+const store = configureStore({
+  reducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(),
+  local,
+});
+
+export default store;
